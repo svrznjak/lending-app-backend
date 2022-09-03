f769c35b5b1ad2761eb81a2ac08121cb43a25c27
gregor <gregor.svrznjak@gmail.com>
2022-09-03 10:11:10 +0200

    Added Transaction mongoose schema

---
+++ src/api/db/schema/Transaction.ts
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const TransactionAddressSchema = new mongoose.Schema({
+  datatype: {
+    type: String,
+    enum: ['budget', 'loan', 'interest', 'outside'],
+    required: true,
+  },
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    index: true,
+  },
+});
+
+const TransactionSchema = new mongoose.Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    index: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    index: true,
+  },
+  transactionTimestamp: { type: Number, required: true },
+  description: { type: String, required: true },
+  from: { type: TransactionAddressSchema, required: true },
+  to: { type: TransactionAddressSchema, required: true },
+  amount: { type: Number, required: true },
+  entryTimestamp: { type: Number, required: true },
+});
+
+TransactionSchema.add({ revisions: [TransactionSchema] });
+export default TransactionSchema;
