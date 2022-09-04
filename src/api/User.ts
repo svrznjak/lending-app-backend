d7a1251090c61e3804bc80dd74096c88dac164c2
gregor <gregor.svrznjak@gmail.com>
2022-09-04 09:31:42 +0200

    Added empty functions definitions

--- src/api/User.ts
+++ src/api/User.ts
@@ -1,7 +1,29 @@
 import mongoose from 'mongoose';
 
 import UserModel from './db/model/UserModel.js';
-import { User } from './interfaces/User.js';
+import { User, NewUserInput, UpdateUserInput } from './interfaces/User.js';
+
+// As a lender, I want to create a user account, so that I can persist changes.
+export async function createNewUser(newUserInfo: NewUserInput): Promise<User> {
+  throw new Error('Function not implemented');
+}
+
+// As a lender, I want to view my user account information, so that I can make appropriate changes.
+export async function getUserById(id: object): Promise<User> {
+  throw new Error('Function not implemented');
+}
+
+// As a lender, I want to change my user account name, so that I can fix errors in spelling.
+// As a lender, I want to change my subscription, so that I can pay for exactly what I need.
+// As a lender, I want to change the UI language, so that I can more easily use the application.
+// As a lender, I want to change my timezone, so that interest can be calculated at the right time (local midnight)
+export async function updateUserById(
+  updatedUserInfo: UpdateUserInput,
+): Promise<User> {
+  throw new Error('Function not implemented');
+}
+
+// example function
 
 export async function createNewUserTest(): Promise<boolean> {
   const newUserData: User = {
