c08886f97c35c66c7b7bd4d905caec00f968a7a5
gregor <gregor.svrznjak@gmail.com>
2022-09-04 12:46:12 +0200

    Added validators for user

--- src/api/types/validators/user.ts
+++ src/api/types/validators/user.ts
@@ -1,23 +1,39 @@
 import validator from 'validator';
 import _ from 'lodash';
 
-import { NewUserInput } from '../interfaces/User.js';
+import { NewUserInput, UpdateUserInput } from '../interfaces/User.js';
 
 export function validateNewUserInput(newUserInput: NewUserInput): NewUserInput {
   const validatedNewUserInput: NewUserInput = _.cloneDeep(newUserInput);
 
+  validatedNewUserInput.name = validateUserName(validatedNewUserInput.name);
+  validatedNewUserInput.email = validateUserEmail(validatedNewUserInput.email);
+  validatedNewUserInput.currency = validateUserCurrency(validatedNewUserInput.currency);
+  validatedNewUserInput.language = validateUserLanguage(validatedNewUserInput.language);
+
   return validatedNewUserInput;
 }
 
+export function validateUpdateUserInput(updateUserInput: UpdateUserInput): UpdateUserInput {
+  const validatedUpdateUserInput: UpdateUserInput = _.cloneDeep(updateUserInput);
+
+  if (validatedUpdateUserInput.name !== undefined)
+    validatedUpdateUserInput.name = validateUserName(validatedUpdateUserInput.name);
+  if (validatedUpdateUserInput.currency !== undefined)
+    validatedUpdateUserInput.currency = validateUserCurrency(validatedUpdateUserInput.currency);
+  if (validatedUpdateUserInput.language !== undefined)
+    validatedUpdateUserInput.language = validateUserLanguage(validatedUpdateUserInput.language);
+
+  return validatedUpdateUserInput;
+}
+
 export function validateUserName(name: string): string {
-  if (!validator.isEmpty(name, { ignore_whitespace: true }))
-    throw new Error('(validation) Name should contain value!');
+  if (!validator.isEmpty(name, { ignore_whitespace: true })) throw new Error('(validation) Name should contain value!');
   return name;
 }
 
 export function validateUserEmail(email: string): string {
-  if (!validator.isEmail(email))
-    throw new Error('(validation) Email value does not contain email!');
+  if (!validator.isEmail(email)) throw new Error('(validation) Email value does not contain email!');
   return email;
 }
 
@@ -30,7 +46,6 @@ export function validateUserCurrency(currency: string): string {
 }
 
 export function validateUserLanguage(language: string): string {
-  if (!validator.isLocale(language))
-    throw new Error('(validation) Language should be locale value!');
+  if (!validator.isLocale(language)) throw new Error('(validation) Language should be locale value!');
   return language;
 }
