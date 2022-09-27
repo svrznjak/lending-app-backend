8f9cac106023bda8ad66dba90534a98df175cdd8
gregor <gregor.svrznjak@gmail.com>
2022-09-27 16:12:12 +0200

    Safe process exitHangled

--- src/main.ts
+++ src/main.ts
@@ -31,3 +31,27 @@ app.use(
 );
 
 app.listen(9000);
+
+process.stdin.resume(); //so the program will not close instantly
+
+function exitHandler(options, exitCode) {
+  console.log('connection close');
+  mongoose.disconnect();
+  // mongoose.connection.close();
+  if (options.cleanup) console.log('clean');
+  if (exitCode || exitCode === 0) console.log(exitCode);
+  if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
