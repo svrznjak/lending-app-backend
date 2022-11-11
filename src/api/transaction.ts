ba941920f5f419068844bb4d9d477eead4124ee9
svrznjak <gregor.svrznjak@gmail.com>
2022-11-11 08:43:16 +0100

    Added chackIfTransactionCanExists

--- src/api/transaction.ts
+++ src/api/transaction.ts
@@ -9,6 +9,8 @@ import BudgetModel from './db/model/BudgetModel.js';
 import Loan from './loan.js';
 import LoanModel from './db/model/LoanModel.js';
 import LoanCache from './cache/loanCache.js';
+import paranoidCalculator from './utils/paranoidCalculator/paranoidCalculator.js';
+import { ILoan } from './types/loan/loanInterface.js';
 
 export default {
   getOne: async function getOneTransaction({ transactionId }: { transactionId: string }): Promise<ITransaction> {
@@ -51,6 +53,12 @@ export default {
       revisions: undefined,
     } as ITransaction;
     transactionHelpers.runtimeCast(validatedTransaction);
+    try {
+      await this.checkIfTransactionCanExist({ transaction: validatedTransaction });
+    } catch (err) {
+      console.log(err);
+      throw new Error(err);
+    }
     try {
       const newTransactionInDB: any = await new TransactionModel(validatedTransaction).save(options);
       const newTransaction: ITransaction = {
@@ -95,6 +103,13 @@ export default {
     transaction.description = newTransaction.description;
     transaction.amount = newTransaction.amount;
     transaction.entryTimestamp = newTransaction.entryTimestamp;
+
+    try {
+      await this.checkIfTransactionCanExist({ transaction: transaction, originalTransactionId: transactionId });
+    } catch (err) {
+      console.log(err);
+      throw new Error(err);
+    }
 
     // Save edited transaction into DB
     await transaction.save();
@@ -297,5 +312,180 @@ export default {
       options,
     );
     return newTransaction;
+  },
+  /**
+   * Checks if transaction will break any of following rules:
+   * - Rule 1: Transaction involving the budget can not occur if the budget would reach negative value at any point in
+   *   time of existance of the budget
+   * - Rule 2: Transaction involving the loan can not occur before the loan is created
+   * - Rule 3: Transaction involving the loan can not occur if the loan has status of "closed"
+   * - Rule 4: Transaction involving the loan can not occur if the loan would get overpaid at any point in time of
+   *   existance of the loan
+   * @param  {ITransaction} transaction - Transaction that is about to be entered into database.
+   * @param  {string|undefined} originalTransactionId - (Used when editing existing transaction) Id of transaction that is being replaced/edited
+   * @returns Promise<true> or throws error
+   */
+  checkIfTransactionCanExist: async function checkIfTransactionCanExist({
+    transaction,
+    originalTransactionId,
+  }: {
+    transaction: ITransaction;
+    originalTransactionId: string | undefined;
+  }): Promise<true> {
+    // Do check for Rule 1
+    if (transaction.from.datatype === 'BUDGET') {
+      const AFFECTED_BUDGET_ID = transaction.from.addressId;
+      const AFFECTED_BUDGET_TRANSACTIONS: ITransaction[] = await Budget.getTransactions(AFFECTED_BUDGET_ID, {
+        pageNumber: 0,
+        pageSize: Infinity,
+      });
+
+      // delete old transaction from Transactions if originalTransactionId is provided
+      if (originalTransactionId !== undefined) {
+        for (let i = 0; i < AFFECTED_BUDGET_TRANSACTIONS.length; i++) {
+          if (AFFECTED_BUDGET_TRANSACTIONS[i]._id === originalTransactionId) {
+            AFFECTED_BUDGET_TRANSACTIONS.splice(i, 1);
+            break;
+          }
+        }
+      }
+
+      // insert new transaction into Transactions at correct index (according to transactionTimestamp and entryTimestamp)
+      /*
+        Algo explained:
+        Loop existing transactions from newest to oldest. (newest transaction = o | oldest transaction = transactions.length-1)
+        If new transaction is newer than transactions[i] then insert new transaction after transactions[i] and break loop.
+        Else if new transaction is equal age as transactions[i] then check for entryTimestamp
+        - if entryTimestamp of new transaction is newer or equal then insert new transaction after transactions[i]
+        - else insert new transaction before transactions[i]
+        If new transaction is not inserted while looping throught transactions 
+        (aka. new transaction is older than oldest transaction)
+        then insert new transaction at end of transactions array (oldest transaction)
+      */
+      let isTransactionInserted = false;
+      for (let i = 0; i < AFFECTED_BUDGET_TRANSACTIONS.length; i++) {
+        if (AFFECTED_BUDGET_TRANSACTIONS[i].transactionTimestamp < transaction.transactionTimestamp) {
+          AFFECTED_BUDGET_TRANSACTIONS.splice(i, 0, transaction);
+          isTransactionInserted = true;
+          break;
+        } else if (AFFECTED_BUDGET_TRANSACTIONS[i].transactionTimestamp === transaction.transactionTimestamp) {
+          if (AFFECTED_BUDGET_TRANSACTIONS[i].entryTimestamp <= transaction.entryTimestamp) {
+            AFFECTED_BUDGET_TRANSACTIONS.splice(i, 0, transaction);
+          } else {
+            AFFECTED_BUDGET_TRANSACTIONS.splice(i + 1, 0, transaction);
+          }
+          isTransactionInserted = true;
+          break;
+        }
+      }
+      if (!isTransactionInserted) {
+        AFFECTED_BUDGET_TRANSACTIONS.splice(AFFECTED_BUDGET_TRANSACTIONS.length - 1, 0, transaction);
+      }
+
+      let budgetFunds = 0;
+      for (let i = AFFECTED_BUDGET_TRANSACTIONS.length - 1; i >= 0; i--) {
+        const TRANSACTION = AFFECTED_BUDGET_TRANSACTIONS[i];
+
+        if (TRANSACTION.from.datatype === 'BUDGET') {
+          budgetFunds = paranoidCalculator.subtract(budgetFunds, TRANSACTION.amount);
+        } else if (TRANSACTION.to.datatype === 'BUDGET') {
+          budgetFunds = paranoidCalculator.add(budgetFunds, TRANSACTION.amount);
+        }
+        if (budgetFunds < 0) {
+          throw new Error(
+            'New transaction would make budget funds negative at (timestamp): ' + TRANSACTION.entryTimestamp,
+          );
+        }
+      }
+    }
+
+    // Do check for Rule 2 and Rule 3
+    if (transaction.from.datatype === 'LOAN') {
+      const AFFECTED_LOAN: ILoan = await Loan.getOneFromUser({
+        userId: transaction.userId,
+        loanId: transaction.from.addressId,
+      });
+      if (AFFECTED_LOAN.openedTimestamp > transaction.transactionTimestamp)
+        throw new Error('Transaction can not occur before loan openedTimestamp');
+
+      if (AFFECTED_LOAN.status === 'CLOSED') throw new Error('Transaction can not occur on loan with status "CLOSED"');
+    } else if (transaction.to.datatype === 'LOAN') {
+      const AFFECTED_LOAN: ILoan = await Loan.getOneFromUser({
+        userId: transaction.userId,
+        loanId: transaction.to.addressId,
+      });
+      if (AFFECTED_LOAN.openedTimestamp > transaction.transactionTimestamp)
+        throw new Error('Transaction can not occur before loan openedTimestamp');
+      if (AFFECTED_LOAN.status === 'CLOSED') throw new Error('Transaction can not occur on loan with status "CLOSED"');
+    }
+
+    // Do check for Rule 4
+    if (transaction.from.datatype === 'LOAN') {
+      const AFFECTED_LOAN = await Loan.getOneFromUser({
+        userId: transaction.userId,
+        loanId: transaction.from.addressId,
+      });
+      const AFFECTED_LOAN_TRANSACTIONS: ITransaction[] = await Loan.getTransactions(AFFECTED_LOAN._id, {
+        pageNumber: 0,
+        pageSize: Infinity,
+      });
+
+      // delete old transaction from Transactions if originalTransactionId is provided
+      if (originalTransactionId !== undefined) {
+        for (let i = 0; i < AFFECTED_LOAN_TRANSACTIONS.length; i++) {
+          if (AFFECTED_LOAN_TRANSACTIONS[i]._id === originalTransactionId) {
+            AFFECTED_LOAN_TRANSACTIONS.splice(i, 1);
+            break;
+          }
+        }
+      }
+
+      // insert new transaction into Transactions at correct index (according to transactionTimestamp and entryTimestamp)
+      /*
+        Algo explained:
+        Loop existing transactions from newest to oldest. (newest transaction = o | oldest transaction = transactions.length-1)
+        If new transaction is newer than transactions[i] then insert new transaction after transactions[i] and break loop.
+        Else if new transaction is equal age as transactions[i] then check for entryTimestamp
+        - if entryTimestamp of new transaction is newer or equal then insert new transaction after transactions[i]
+        - else insert new transaction before transactions[i]
+        If new transaction is not inserted while looping throught transactions 
+        (aka. new transaction is older than oldest transaction)
+        then insert new transaction at end of transactions array (oldest transaction)
+      */
+      let isTransactionInserted = false;
+      for (let i = 0; i < AFFECTED_LOAN_TRANSACTIONS.length; i++) {
+        if (AFFECTED_LOAN_TRANSACTIONS[i].transactionTimestamp < transaction.transactionTimestamp) {
+          AFFECTED_LOAN_TRANSACTIONS.splice(i, 0, transaction);
+          isTransactionInserted = true;
+          break;
+        } else if (AFFECTED_LOAN_TRANSACTIONS[i].transactionTimestamp === transaction.transactionTimestamp) {
+          if (AFFECTED_LOAN_TRANSACTIONS[i].entryTimestamp <= transaction.entryTimestamp) {
+            AFFECTED_LOAN_TRANSACTIONS.splice(i, 0, transaction);
+          } else {
+            AFFECTED_LOAN_TRANSACTIONS.splice(i + 1, 0, transaction);
+          }
+          isTransactionInserted = true;
+          break;
+        }
+      }
+      if (!isTransactionInserted) {
+        AFFECTED_LOAN_TRANSACTIONS.splice(AFFECTED_LOAN_TRANSACTIONS.length - 1, 0, transaction);
+      }
+
+      const TRANSACTIONS_LIST = Loan.generateTransactionsList({
+        loanTransactions: AFFECTED_LOAN_TRANSACTIONS,
+        interestRate: AFFECTED_LOAN.interestRate,
+      });
+
+      for (let i = 0; i < TRANSACTIONS_LIST.length; i++) {
+        if (TRANSACTIONS_LIST[i].outstandingPrincipal < 0) {
+          throw new Error(
+            'New transaction would make loan outstanding principal negative at (timestamp): ' +
+              TRANSACTIONS_LIST[i].toDateTimestamp,
+          );
+        }
+      }
+    }
+    return true;
   },
 };
