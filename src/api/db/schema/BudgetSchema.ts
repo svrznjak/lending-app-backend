21c3cc95262e33b8bf46aba2b0e239d5fe31c2cf
svrznjak <gregor.svrznjak@gmail.com>
2022-11-28 22:52:12 +0100

    Added toSimple() on mongo document

--- src/api/db/schema/BudgetSchema.ts
+++ src/api/db/schema/BudgetSchema.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { IBudget } from '../../types/budget/budgetInterface.js';
 import existsOneWithId from '../plugins/existsOneWithId.js';
 
 import { LoanInterestRateSchema } from './LoanInterestRateSchema.js';
@@ -12,6 +13,7 @@ export const BudgetSchema = new mongoose.Schema(
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
+      ref: 'Users',
     },
     name: { type: String, required: true },
     description: { type: String, required: true },
@@ -24,6 +26,41 @@ export const BudgetSchema = new mongoose.Schema(
   {
     statics: {
       existsOneWithId,
+    },
+    methods: {
+      toSimple: function toSimple(): IBudget {
+        const MONGO_BUDGET: any = this.toObject();
+        return {
+          _id: MONGO_BUDGET._id.toString(),
+          userId: MONGO_BUDGET.userId.toString(),
+          name: MONGO_BUDGET.name,
+          description: MONGO_BUDGET.description,
+          defaultInterestRate: {
+            type: MONGO_BUDGET.defaultInterestRate.type,
+            duration: MONGO_BUDGET.defaultInterestRate.duration,
+            expectedPayments: MONGO_BUDGET.defaultInterestRate.expectedPayments,
+            amount: MONGO_BUDGET.defaultInterestRate.amount,
+            isCompounding: MONGO_BUDGET.defaultInterestRate.isCompounding,
+            entryTimestamp: MONGO_BUDGET.defaultInterestRate.entryTimestamp,
+            revisions:
+              MONGO_BUDGET.defaultInterestRate.revisions !== undefined
+                ? {
+                    type: MONGO_BUDGET.defaultInterestRate.revisions.type,
+                    duration: MONGO_BUDGET.defaultInterestRate.revisions.duration,
+                    expectedPayments: MONGO_BUDGET.defaultInterestRate.revisions.expectedPayments,
+                    amount: MONGO_BUDGET.defaultInterestRate.revisions.amount,
+                    isCompounding: MONGO_BUDGET.defaultInterestRate.revisions.isCompounding,
+                    entryTimestamp: MONGO_BUDGET.defaultInterestRate.revisions.entryTimestamp,
+                    revisions: MONGO_BUDGET.defaultInterestRate.revisions.revisions,
+                  }
+                : undefined,
+          },
+          calculatedTotalInvestedAmount: MONGO_BUDGET.calculatedTotalInvestedAmount,
+          calculatedTotalWithdrawnAmount: MONGO_BUDGET.calculatedTotalWithdrawnAmount,
+          calculatedTotalAvailableAmount: MONGO_BUDGET.calculatedTotalAvailableAmount,
+          isArchived: MONGO_BUDGET.isArchived,
+        };
+      },
     },
   },
 );
