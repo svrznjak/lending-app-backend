0af36917285f2d9f9ab346a6418da9444093658d
svrznjak <gregor.svrznjak@gmail.com>
2023-01-05 17:46:00 +0100

    Trying to fix production build error

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+export default {
+  type: 'service_account',
+  project_id: 'lender-app-e3988',
+  private_key_id: '02f2142a66175d6ff8f480340f943b9c64ab6c0e',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDLq5HoLJj3guql\n99qEYa7AsErJnZOj3EjxpIuaFepDAVTW9ANvhlTEuAwwg8ESgmaoe7eyfZCAaJZH\nUBCnyYAYHxFGA+FmGSHPCurMhnDBcxRu6FQkPJ2oLDFr64/5p78qJXsrsS2jBc7H\njAIi7bwqGgyy85AIQVxX9uWvsuEHgTyN+JiswNTZjNewkjiiXx+Jb8iEhW7NNAxQ\nQO+swh45kiuqzqDer8pnR2J0u08u+R5CEUk6Gg3/hUWX0WI8KoUdUG29SpgqZYKl\n/Ix4lwVHkxLp+HiLKM/kDnn1P3rTGzgKw9Ef9oq+KI9JSRKqWhFkoufNfpEKYoQm\nVlufDwonAgMBAAECggEAARDLLmnLixcWQsJCxjNthmpXfWKOV2qwvwLpmr0AZIx3\nPA7l4wl9jdO2BYEuhhZ9dh9RXSwnAoFtIoZh03taocWtLFlFvNnnCH4PPcQI+MTN\nl60uLGiVq3ixf7l+rcqZx3JzMzFSWAbmMkkUIcht+ZmHEbQs+IUylH0EpxIDaSu+\nNMpWjeNC6z0e8b5rmj6DCSMD+Cj9kVinCrpzVwbci6sYGdSlf7taEtkNsGoxkuYn\nEmsfuT+lJP3hfFmNvdgb61e7WKEV/OHAnGARU3f138LN99a1pKJH+XzBKvB15gqZ\nVNZ9S4SO7X4KGWEdRr4JpTxvKr8rus+aisWUNSTS4QKBgQDqmC88tbQaME7/ngFY\ngcSgwl8edG6N6xjHzMjoL0QpHQg00ZlOM2kNnW08sGR2SLF72IPi3SlgQonpI4Qc\nN8qrDMIN9b600jxrUT1vSCZLmCrLaQyuhMSgM9JrODW3KbBsIo2+r5a17OpD1O/e\nhKWZh+boYS9lGY0R2xqPap/YuQKBgQDeQQkPekG3TSGrIPoNlqV8OC2JVI60AHg4\nM2GUkpQC0XwIvXDjOfzi9vfGw+3VmFcV9RpRWJmdK2BgQ5PiFzvcIhJBpWDJ0Xgc\ni3z5CP0UJgZDNRd6i9cKMxdmZuBTIamlq58o/1U2Vb9YqdxyeII9Vx3qd1bHlBb3\nZ9iD/ejJ3wKBgFPvDzeRa1bTYfBi/+hCxa8e3BlxXvy32K3jvx4TizeKg50iQM+R\n5UxRvvuvwMHxUE8XMFjC+ELQi7c2VdYiwAhjECseeZUIg0WCU5C1J750Oxt8AmWO\naytGX0qtWcqmx9BorH5FJI8CrO6ICSp8GyPLU/pgiyyI78grtF0LsRVBAoGAKver\nxsA+AC/7OFhRv1rGd3Ns6kXcqNQGaecVEGIeF0Vkk1P8hRFq4H4afOTYEBk5jNcr\n1VopqIcARNwLtQQE4JP9r49HXP0QGYSmDoc8Vi7buxaCiYCLfD9QYM22xJuXbp31\nUTm7wZtmOL/uDHtbwnwbHn8f2cm5fu8mnq3x2UUCgYEApLyqkJ701SrgS4F3zK1Z\nxJGFvMWGf1hKdpbuLhzkKSRUeOoygoyiX54ZKJ3zx62CC2QCIrnHlKv26OhtHszP\n2Rg/iNX/We9CjlBpCVUFKo2JSBOYQzRLG0ZdRCgCeLt2gYQvUmpZxOddBzyY9t+d\ngQHeBAJjfgnL2sMhIyjCKpY=\n-----END PRIVATE KEY-----\n',
+  client_email: 'firebase-adminsdk-3trr9@lender-app-e3988.iam.gserviceaccount.com',
+  client_id: '103224484370001876297',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-3trr9%40lender-app-e3988.iam.gserviceaccount.com',
+};
