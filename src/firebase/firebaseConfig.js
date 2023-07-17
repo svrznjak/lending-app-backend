cc975573c03f80bd660f6a48ca6d9a4fcc93fd46
svrznjak <gregor.svrznjak@gmail.com>
2023-07-17 11:38:02 +0200

    Adds .env supported firebaseConfig.js

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+export default {
+  type: process.env.FB_TYPE,
+  project_id: FB_PROJECT_ID,
+  private_key_id: FB_PRIVATE_KEY_ID,
+  private_key: FB_PRIVATE_KEY,
+  client_email: FB_CLIENT_EMAIL,
+  client_id: FB_CLIENT_ID,
+  auth_uri: FB_AUTH_URI,
+  token_uri: FB_TOKEN_URI,
+  auth_provider_x509_cert_url: FB_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: FB_CLIENT_X509_CERT_URL,
+  universe_domain: FB_UNIVERSE_DOMAIN,
+};
