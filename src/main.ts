194bad225dd87c58875d1b669d6db22fb7b152f5
svrznjak <gregor.svrznjak@gmail.com>
2024-03-11 02:28:13 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -24,10 +24,7 @@ import budget from './api/budget.js';
 // where the GraphQL over HTTP fastify request handler is
 const fastify = Fastify();
 // allow localhost client to connect
-await fastify.register(fastcors, {
-  origin: ['https://money-lender.app', 'http://localhost:5173'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-});
+await fastify.register(fastcors);
 
 // mount the GraphQL over HTTP fastify request handler on `/graphql` and inlude the schema and context
 fastify.all(
