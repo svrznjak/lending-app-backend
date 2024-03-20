6b8cbcfc2437b5dbbb39be167f76fd44b87107f5
svrznjak <gregor.svrznjak@gmail.com>
2024-03-20 14:42:32 +0100

    Android capacitor cors

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ import configureHttpRoutes from './http/configureHttpRoutes.js';
 const fastify = Fastify();
 // allow localhost client to connect
 await fastify.register(fastcors, {
-  origin: ['https://money-lender.app', 'http://localhost:5173'],
+  origin: ['https://money-lender.app', 'http://localhost:5173', 'http://localhost'],
   methods: 'GET,HEAD,PUT,PATCH,OPTIONS,POST,DELETE',
   allowedHeaders: 'Content-Type, Authorization, if-none-match',
   exposedHeaders: 'ETag',
